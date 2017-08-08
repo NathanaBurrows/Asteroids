83cc7b1da5806a9d59035ced6903c81d7495fd34
NathanaBurrows <neonsiege12@hotmail.com>
2017-08-08 11:10:13 +1000

    Updating Asteroids game project file

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int lives = 3;
+    public float rotationSpeed = 2;
+    public float movementSpeed = 5;
+    public float acceleration = 50f;
+    public float deceleration = .1f;
+
+    private Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If user presses W
+        if (Input.GetKey(KeyCode.W)){
+            rigid.AddForce(transform.right * acceleration);}
+        if (Input.GetKey(KeyCode.A)){
+            rigid.AddForce(transform.up * acceleration);}
+        if (Input.GetKey(KeyCode.S)){
+            rigid.AddForce(-transform.right * acceleration);}
+        if (Input.GetKey(KeyCode.D)){
+            rigid.AddForce(-transform.up * acceleration);}
+
+        if (Input.GetKey(KeyCode.A)){
+            transform.rotation *= Quaternion.AngleAxis(5, Vector3.forward);}
+        if (Input.GetKey(KeyCode.D)){
+            transform.rotation *= Quaternion.AngleAxis(-5, Vector3.forward);}
+
+        rigid.velocity += -rigid.velocity * deceleration;
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Functions
+{
+
+    public class Shooting : MonoBehaviour
+    {
+        //Stores the object we want to Instantiate
+        public GameObject projectilePrefab;
+        //Speed at whuch the projectile will be flung
+        public float projectileSpeed = 20f;
+        //Rate of fire
+        public float shootRate = 0.1f;
+        //Timer to count up to shootRate
+        private float shootTimer = 0f;
+        //Container for the RigidBody2D
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            //Get the rigidbody component
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Increase the timer
+            shootTimer += Time.deltaTime;
+            // AND                      = '&&'
+            // OR                       = '||'
+            // Greater than or equal to = '>='
+            // Less than or equal to    = '<='
+            // Not equal to             = '!='
+            // Is equal to              = '=='
+            //IF space bar is pressed AND shootTimer >= shootRate
+            if (Input.GetKey(KeyCode.Space) && shootTimer >= shootRate)
+            {
+                //CALL Shoot()
+                Shoot();
+                //RESET shootTimer = 0
+                shootTimer = 0;
+            }
+        }
+        void Shoot()
+        {
+            print("shooting projectile");
+
+            // Instantiate a projectile
+            Instantiate(projectilePrefab);
+            // Send projectile flying forward
+            // Apply recoil to player
+        }
+    }
+}
